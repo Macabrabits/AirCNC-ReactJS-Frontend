0eb974b20846a97dcc8a4403271e6b055e6e32eb
Victor Mattar <suporte@trustit.com.br>
2019-10-13 13:08:58 -0300

    finishing dashboard screen

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import api from '../../services/api'
 import './styles.css'
 
@@ -24,9 +25,13 @@ export default function Dashboard(){
                     <header style={{ backgroundImage: `url(${spot.thumbnail_url})`}}></header>
                     <strong>{spot.company}</strong>
                     <span>{spot.price ? `R$${spot.price}/dia` : 'GRATUITO'}</span>                
-                </li>
+                </li> 
             ))}
         </ul>
+
+        <Link to='/new'>
+            <button className="btn">Cadastrar Spot</button>
+        </Link>
         </>
     )
 }
